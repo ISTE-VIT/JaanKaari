2534d35680e42014da5df47c3c8e6d0a9732f9f6
Manasvi Alimchandani <manasvi.alimchandani2019@vitstudent.ac.in>
2020-05-01 01:12:50 +0530

    connected blog page with login

---
+++ routes/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+const post = require('../models/posts')
+const MarioChar = require('../models/mariochar');
+const { ensureAuthenticated } = require('../config/auth');
+
+
+
+router.get('/',function(req,res){
+    res.render('welcome');
+})
+ router.get(`/dashboard`, ensureAuthenticated,function(req,res){
+     res.render('welcome',{user:req.user});
+ })
+
+router.get('/store',ensureAuthenticated,function(req,res){
+    res.render('store');
+})
+//blogs part
+router.get("/blog",function(request,response){
+    
+    post.find({},function(err,foundtpost){
+        
+            if(err)
+            console.log(err);
+            else
+            response.render("blog",{post: foundtpost});
+        });
+    
+});
+router.get("/post/:id",ensureAuthenticated,function(request,response){
+    const search=request.params.id;
+    post.findOne({_id: search},function(err,finditem){
+       response.render("posts",{title: finditem.title,postBody: finditem.postBody,picname: finditem.picname,author:finditem.authorname});
+    });
+});
+
+router.get('/contribute',function(req,res){
+    res.render('contribute');
+})
+
+module.exports = router;
---
+++ routes/publisher.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const post = require('../models/posts')
+const MarioChar = require('../models/mariochar');
+const { ensureAuthenticated } = require('../config/auth');
+
+
+
+// router.get('/dashboard',function(req,res){
+//     res.render('dashboard',{user:req.user});
+// })
+router.get(`/dashboard`,async function(req,res){
+    
+    // let user = await MarioChar.findById(req.params.id)
+   
+    res.render('dashboard',{user:req.user});
+})
+
+
+router.get('/store',function(req,res){
+    res.render('store');
+})
+router.get('/contribute',function(req,res){
+    res.render('contribute');
+})
+
+
+module.exports = router;

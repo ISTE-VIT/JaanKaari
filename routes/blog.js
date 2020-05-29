9e207421ccc0adbd6feec323f9bc3f443aca28a9
Manasvi070902 <manasvi.alimchandani2019@vitstudent.ac.in>
2020-05-29 16:05:44 -0700

    improved blog part

--- routes/blog.js
+++ routes/blog.js
@@ -23,18 +23,13 @@ const upload= multer({
 
  router.use(bodyParser.urlencoded({extended: true}));
 
-
+router.use('/',express.static(__dirname+"/roots"));
 router.use('/:id',express.static("./public")); 
 router.use('/',express.static("./public")); 
 
-router.get("/:id/blog",function(request,response){
-    post.find({},function(err,foundtpost){
-        
-            if(err)
-            console.log(err);
-            else
-            response.render("blog",{post: foundtpost});
-        });
+router.get("/:id/blog", async (req,res)=>{
+    const foundpost=await post.find().sort({update: 'desc'})
+    res.render('blog',{post: foundpost})
     
 });
 
@@ -100,7 +95,7 @@ router.post("/:id/compose",async(req,res) => {
      const search=request.params.postid;
      
      post.findOne({_id: search},function(err,finditem){
-        response.render("posts",{title: finditem.title,postBody: finditem.postBody,picname: finditem.picname,author:finditem.authorname});
+        response.render("viewpost",{title: finditem.title,postBody: finditem.postBody,picname: finditem.picname,author:finditem.authorname});
      });
  });
 router.post("/:id/delpost",function(request,response){
@@ -181,80 +176,56 @@ router.post("/:id/updatedpost",function(req,res){
 });
 
 
-
 router.get("/:id/account",async function(request,response){
     var search=request.params.id;
+    var user = request.user
     await MarioChar.findOne({_id:search}).then(user=>{
         var author_name =  user.username
+       
         
-
-    
     post.find({authorname:author_name},function(err,foundtpost){
         
             if(err)
             console.log(err);
             else
-            response.render("doc",{post: foundtpost});
+            response.render("doc",{post: foundtpost,docname:author_name});
     });   
 
    
-})   
-
+}) ;  
 });
+
+
 
 //subscribe
 
-router.post("/:id/subscribe",async(req,res) => {
-    var author_id=req.params.id;
-    
-    await MarioChar.findOne({_id:author_id}).then(user=>{
-        var author_name =  user.username
-        // console.log(author_name)
-    
-    console.log(author_id)
-    var d = new Date();
-    var date = d.getDate();
-    var month = d.getMonth() + 1;
-    var year = d.getFullYear();
-    var dateStr = date + "/" + month + "/" + year;
+// router.post("/:id/post/subscribe",async(req,res) => {
+//     var author_id=req.params.postid;
+//     var subs_id = req.params.id
 
-    upload(req,res, async(err) => {
-        if(err){
-            res.render("dashboard",{
-                msg: err
-            });
-        }
-        else{
-             const newpost=await new post({
-                // _id:getNextSequence("userid"),
-                title: req.body.posttitle,
-                postBody: req.body.postbody,
-                update:  dateStr,
-                picname: req.file.filename,
-                 authorid: author_id,
-                 authorname:author_name,
-                 
-                
-               
-                
-            });
-            newpost.save(function(err){
-                if(!err)
-                {
-                   
-
-                res.redirect(`./account`);
-                }   else{
-                    console.log(err)
-                    };
-                
-             });  
-        }
-    });
-});
-   
     
-}); 
+//     await MarioChar.findOne({_id:author_id}).then(user=>{
+       
+//         user.subscriber=subs_id
+
+        
+//          console.log(subs_id)
+
+//         Mariochar.save(function(err){
+//                 if(!err)
+//                 {
+//                    res.redirect(`./blog`);
+//                 }   else{
+//                     console.log(err)
+//                     };
+                
+//              });  
+        
+//     });
+    
+            
+         
+// }); 
 
 
 module.exports = router;
